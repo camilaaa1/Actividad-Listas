d207fa939701ee864217f997288fb154e87447da
camilaaa1 <115585056+camilaaa1@users.noreply.github.com>
2022-11-01 13:47:27 -0500

    Add files via upload

---
+++ Listas/Uno.py
@@ -0,0 +1,23 @@
+paises = ['Japón','Estados Unidos','Noruega', 'Canadá','Nueva Zelanda']
+print (paises)
+
+print (sorted(paises))
+
+print (paises)
+
+x = sorted(paises, reverse = True)
+print (x)
+
+print (paises)
+
+paises.reverse()
+print (paises)
+
+paises.reverse()
+print (paises)
+
+paises.sort()
+print (paises)
+
+paises.sort(reverse=True)
+print (paises)
